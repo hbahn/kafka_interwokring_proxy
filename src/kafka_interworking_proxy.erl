647310de13b9900410a091a3e45a75027b47132b
Hongbeom Ahn <hb.ahn@sktelecom.com>
2020-07-27 15:33:15 +0900

    change filtering rule

--- src/kafka_interworking_proxy.erl
+++ src/kafka_interworking_proxy.erl
@@ -195,7 +195,7 @@ on_message_publish(Message = #message{topic = Topic, payload = Payload, qos = Qo
     Checker2 = string:find(From, "smart-fleet-vse-ex-cits-alert-") =:= From,
     Checker3 = string:find(From, "smart-fleet-ovs-") =:= From,
     Checker4 = string:find(Topic, "ovs/location") =:= Topic,
-    Checker5 = string:find(Username, "tmapclient") =:= From,
+    Checker5 = string:find(From, "tmapclient") =:= From,
 
     
     if
